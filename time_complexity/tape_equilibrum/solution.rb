55f59b452f26f15ae2cfc0e66936d5fc68c04e69
Yacobus Reinhart <yacobus.reinhart@gmail.com>
2014-01-02 00:08:21 +0700

    Create solution.rb

---
+++ time_complexity/tape_equilibrum/solution.rb
@@ -0,0 +1,14 @@
+def solution(a)
+  total_left = 0
+  total_right = a.inject(0){|sum,x| sum += x }
+  result = []
+  
+  a.size.times do |p|
+    total_left += a[p]
+    total_right -= a[p]
+    diff = (total_left - total_right).abs
+    result.push(diff)
+    result.sort!.pop if result.size > 1  
+  end
+  result.first
+end
